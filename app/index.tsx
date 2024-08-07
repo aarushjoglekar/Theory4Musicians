1fd613a4af2524b6ae584c16c038d7466ae24ef9
ClawlessTiger <aarush.joglekar@gmail.com>
2024-08-06 20:39:36 -0700

    icon and splash

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
-import { Redirect } from "expo-router";
+import { Redirect, SplashScreen } from "expo-router";
 
 export default function Index() {
+  SplashScreen.preventAutoHideAsync()
+  setTimeout(SplashScreen.hideAsync,2000)
   return (
     <Redirect href={"/home"}/>
   )
