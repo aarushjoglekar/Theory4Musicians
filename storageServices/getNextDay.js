9acccb78e9dfbe7bc1e61fcbb2b8491de11578b3
ClawlessTiger <aarush.joglekar@gmail.com>
2024-08-17 13:32:06 -0700

    revert?

--- storageServices/getNextDay.js
+++ storageServices/getNextDay.js
@@ -1,5 +1,5 @@
 export default async function getNextDay(recentDate) {
-  const date = `${recentDate[0]} ${recentDate[1]} ${recentDate[2]}`
+  const date = `${recentDate[2]}-${recentDate[0]}-${recentDate[1]}`
   let tomorrow = new Date(new Date(date).setDate(new Date(date).getDate() + 1))
   return [tomorrow.getMonth() + 1, tomorrow.getDate(), tomorrow.getFullYear()]
 }
