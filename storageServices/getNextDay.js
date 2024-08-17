e1572d86c5190f25feaf360b02d8cabd0c0b54be
ClawlessTiger <aarush.joglekar@gmail.com>
2024-08-17 13:29:44 -0700

    bugs with sim

--- storageServices/getNextDay.js
+++ storageServices/getNextDay.js
@@ -1,5 +1,6 @@
 export default async function getNextDay(recentDate) {
-  const date = `${recentDate[0]} ${recentDate[1]} ${recentDate[2]}`
+  const date = `${recentDate[2]}-${recentDate[0]}-${recentDate[1]}`
   let tomorrow = new Date(new Date(date).setDate(new Date(date).getDate() + 1))
+  console.log("TOMORROW: ",[tomorrow.getMonth() + 1, tomorrow.getDate(), tomorrow.getFullYear()])
   return [tomorrow.getMonth() + 1, tomorrow.getDate(), tomorrow.getFullYear()]
 }
